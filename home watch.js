fcfdffecb66832fc27fdbda80f8e57c860a5f84e
jmccune98 <jacemccune47@gmail.com>
2025-01-17 19:13:32 -0500

    Add files via upload

---
+++ home watch.js
@@ -0,0 +1,5 @@
+﻿document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert("Thank you for contacting us! We will get back to you shortly.");
+  // You can also integrate backend API to process the form data
+});
